6e3329860da2c276aae6509319aa29393af4cbff
Vikram Jalali <vikram.jalali@tcs.com>
2019-05-03 18:33:30 +0000

    Changes by Vikram-05/03/2019

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,37 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
+	"sap/ui/core/Fragment",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/resource/ResourceModel"
-], function (Controller, MessageToast, JSONModel, ResourceModel) {
+], function (Controller, MessageToast, JSONModel, ResourceModel, Fragment) {
 	"use strict";
 
 	return Controller.extend("Walkthrough.controller.App", {
 
-	
+		onOpenDialog: function (oEvt) {
+			this.getOwnerComponent().openHelloDialog();
+		//	MessageToast.show("Hello App1");
+			// var oView = this.getView();
 
+			// // create dialog lazily
+			// if (!this.byId("DialogFrag")) {
+			// 	// load asynchronous XML fragment
+			// 	Fragment.load({
+			// 		id: oView.getId(),
+			// 		name: "Walkthrough.view.DepotDirect"
+			// 	}).then(function (oDialog) {
+			// 		// connect dialog to the root view of this component (models, lifecycle)
+			// 		oView.addDependent(oDialog);
+			// 		oDialog.open();
+			// 	});
+			// } else {
+			// 	this.byId("DialogFrag").open();
+			// }
+		},
+		onCloseFrag: function () {
+			this.byId("DialogFrag").close();
+		},
 		onPress: function (oEvt) {
 			MessageToast.show("Hello App");
 		}
